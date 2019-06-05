137c95eefeb8e85d5568b7e39fb7114bd53cf3c3
Amer Alimanović <x@amer8.com>
2019-06-05 18:43:12 +0200

    Add standardjs and fix lint errors

--- index.js
+++ index.js
@@ -1,47 +1,44 @@
-'use strict';
 
-var soWatchify = require('smartobject-watchify');
+const soWatchify = require('smartobject-watchify')
 
-module.exports = function smartbase(so, plugins) {
-    var iidEvents = {};
+module.exports = function smartbase (so, plugins) {
+  const iidEvents = {}
 
-    so = soWatchify(so);
-    so.hal = so.hal || {};
+  so = soWatchify(so)
+  so.hal = so.hal || {}
 
-    // prepare hal and setup functions
-    for (var name in plugins) {
-        if (plugins.hasOwnProperty(name))
-            so.hal[name] = plugins[name].handle;
+  // prepare hal and setup functions
+  for (let name in plugins) {
+    if (plugins.hasOwnProperty(name)) { so.hal[name] = plugins[name].handle }
+  }
+
+  // initialize the smart object
+  for (let name in plugins) {
+    if (plugins.hasOwnProperty(name)) {
+      const plugin = plugins[name]
+      const { oid } = plugin
+      const { iid } = plugin
+      const { resources } = plugin
+      const { setup } = plugin
+      const iidEvent = `${oid}/${iid}/`
+
+      iidEvents[iidEvent] = name
+
+      so.init(oid, iid, resources, function () {
+        if (typeof setup === 'function') { setup.call(this) }
+      })
     }
+  }
 
-    // initialize the smart object
-    for (var name in plugins) {
-        if (plugins.hasOwnProperty(name)) {
-            var plugin = plugins[name],
-                oid = plugin.oid,
-                iid = plugin.iid,
-                resources = plugin.resources,
-                setup = plugin.setup,
-                iidEvent = oid + '/' + iid + '/';
-
-            iidEvents[iidEvent] = name;
-
-            so.init(oid, iid, resources, function () {
-                if (typeof setup === 'function')
-                    setup.call(this);
-            });
-        }
+  for (const ievt in iidEvents) {
+    if (iidEvents.hasOwnProperty(ievt)) {
+      // Bridge to alias event
+      so.onChange(ievt, (rRec) => { // { iPath, rid: rid, cVal: currentValue, pVal: oldValue }
+        const bridgedEvt = `${iidEvents[rRec.iPath]}/${rRec.rid}`
+        so._emit(bridgedEvt, rRec.cVal, rRec.pVal)
+      })
     }
+  }
 
-    for (var ievt in iidEvents) {
-        if (iidEvents.hasOwnProperty(ievt)) {
-            // Bridge to alias event
-            so.onChange(ievt, function (rRec) { // { iPath, rid: rid, cVal: currentValue, pVal: oldValue }
-                var bridgedEvt = iidEvents[rRec.iPath] + '/' + rRec.rid;
-                so._emit(bridgedEvt, rRec.cVal, rRec.pVal);
-            });
-        }
-    };
-
-    return so;
-};
+  return so
+}
--- test/smartbase.test.js
+++ test/smartbase.test.js
@@ -1,152 +1,152 @@
-var chai = require('chai'),
-    expect = chai.expect,
-    SmartObject = require('smartobject'),
-    smartbase = require('../index.js');
+/* eslint-env mocha */
+const chai = require('chai')
 
-describe('Functional Check', function () {
-    var so = new SmartObject(),
-        plugin1 = {
-            oid: 'lightCtrl',
-            iid: 0,
-            handle: {},
-            active: true,
-            setup: null,
-            resources: {
-                onOff: 1
-            }
-        },
-        plugin2 = {
-            oid: 'lightCtrl',
-            iid: 1,
-            handle: {},
-            active: true,
-            setup: null,
-            resources: {
-                onOff: 0
-            }
-        },
-        plugin3 = {
-            oid: 'temperature',
-            iid: 0,
-            handle: {},
-            setup: null,
-            resources: {
-                sensorValue: 30
-            }
-        },
+const { expect } = chai
+const SmartObject = require('smartobject')
+const smartbase = require('../index.js')
 
-    so = smartbase(so, {
-        led0: plugin1,
-        led1: plugin2,
-        temp0: plugin3
-    });
+describe('Functional Check', () => {
+  let so = new SmartObject()
 
-    it('should has object instance lightCtrl/0', function () {
-        expect(so.has('lightCtrl', 0)).to.be.equal(true);
-    });
+  const plugin1 = {
+    oid: 'lightCtrl',
+    iid: 0,
+    handle: {},
+    active: true,
+    setup: null,
+    resources: {
+      onOff: 1
+    }
+  }
 
-    it('should has object instance lightCtrl/1', function () {
-        expect(so.has('lightCtrl', 0)).to.be.equal(true);
-    });
+  const plugin2 = {
+    oid: 'lightCtrl',
+    iid: 1,
+    handle: {},
+    active: true,
+    setup: null,
+    resources: {
+      onOff: 0
+    }
+  }
 
-    it('should has object instance temperature/0', function () {
-        expect(so.has('temperature', 0)).to.be.equal(true);
-    });
+  const plugin3 = {
+    oid: 'temperature',
+    iid: 0,
+    handle: {},
+    setup: null,
+    resources: {
+      sensorValue: 30
+    }
+  }
 
+  so = smartbase(so, {
+    led0: plugin1,
+    led1: plugin2,
+    temp0: plugin3
+  })
 
-    it('should fire and listened via onChange correctly', function (done) {
-        so.onChange('hello', function (data) {
-            if (data === 1)
-                done();
-        });
+  it('should has object instance lightCtrl/0', () => {
+    expect(so.has('lightCtrl', 0)).to.be.equal(true)
+  })
 
-        setImmediate(function () {
-            so._emit('hello', 1);
-        });
-    });
+  it('should has object instance lightCtrl/1', () => {
+    expect(so.has('lightCtrl', 0)).to.be.equal(true)
+  })
 
-    it('should fire and listened via on correctly', function (done) {
-        so.onChange('helloY', function (data) {
-            if (data === 1)
-                done();
-        });
+  it('should has object instance temperature/0', () => {
+    expect(so.has('temperature', 0)).to.be.equal(true)
+  })
 
-        setImmediate(function () {
-            so._emit('helloY', 1);
-        });
-    });
+  it('should fire and listened via onChange correctly', (done) => {
+    so.onChange('hello', (data) => {
+      if (data === 1) { done() }
+    })
 
-    it('should init correctly', function () {
-        expect(so.get('temperature', 0, 'sensorValue')).to.be.equal(30);
-        expect(so.get('lightCtrl', 0, 'onOff')).to.be.equal(1);
-        expect(so.get('lightCtrl', 1, 'onOff')).to.be.equal(0);
-    });
+    setImmediate(() => {
+      so._emit('hello', 1)
+    })
+  })
 
-    it('should report change of temperature/0/sensorValue correctly', function (done) {
-        so.onChange('temperature/0/sensorValue', function (cVal, pVal) {
-            if (cVal === 80 && pVal === 30)
-                done();
-        });
-        so.write('temperature', 0, 'sensorValue', 80, function (err, data) {
-            // console.log(err);
-            // console.log(data);
-        });
-    });
+  it('should fire and listened via on correctly', (done) => {
+    so.onChange('helloY', (data) => {
+      if (data === 1) { done() }
+    })
 
-    it('should report change of temp0/sensorValue correctly', function (done) {
-        so.onChange('temp0/sensorValue', function (cVal, pVal) {
-            if (cVal === 50 && pVal === 80)
-                done();
-        });
-        so.write('temperature', 0, 'sensorValue', 50, function (err, data) {
-            // console.log(err);
-            // console.log(data);
-        });
-    });
+    setImmediate(() => {
+      so._emit('helloY', 1)
+    })
+  })
 
-    it('should report change of lightCtrl/0/onOff correctly', function (done) {
-        so.onChange('lightCtrl/0/onOff', function (cVal, pVal) {
-            if (cVal === 0 && pVal === 1)
-                done();
-        });
-        so.write('lightCtrl', 0, 'onOff', 0, function (err, data) {
-            // console.log(err);
-            // console.log(data);
-        });
-    });
+  it('should init correctly', () => {
+    expect(so.get('temperature', 0, 'sensorValue')).to.be.equal(30)
+    expect(so.get('lightCtrl', 0, 'onOff')).to.be.equal(1)
+    expect(so.get('lightCtrl', 1, 'onOff')).to.be.equal(0)
+  })
 
-    it('should report change of led0/onOff correctly', function (done) {
-        so.onChange('led0/onOff', function (cVal, pVal) {
+  it('should report change of temperature/0/sensorValue correctly', (done) => {
+    so.onChange('temperature/0/sensorValue', (cVal, pVal) => {
+      if (cVal === 80 && pVal === 30) { done() }
+    })
+    so.write('temperature', 0, 'sensorValue', 80, (err, data) => {
+      expect(err).to.be.equal(null)
+      // console.log(err);
+      // console.log(data);
+    })
+  })
 
-            if (cVal === 1 && pVal === 0)
-                done();
-        });
-        so.write('lightCtrl', 0, 'onOff', 1, function (err, data) {
-            // console.log(err);
-            // console.log(data);
-        });
-    });
+  it('should report change of temp0/sensorValue correctly', (done) => {
+    so.onChange('temp0/sensorValue', (cVal, pVal) => {
+      if (cVal === 50 && pVal === 80) { done() }
+    })
+    so.write('temperature', 0, 'sensorValue', 50, (err, data) => {
+      expect(err).to.be.equal(null)
+      // console.log(err);
+      // console.log(data);
+    })
+  })
 
-    it('should report change of lightCtrl/1/onOff correctly', function (done) {
-        so.onChange('lightCtrl/1/onOff', function (cVal, pVal) {
-            if (cVal === 1 && pVal === 0)
-                done();
-        });
-        so.write('lightCtrl', 1, 'onOff', 1, function (err, data) {
-            // console.log(err);
-            // console.log(data);
-        });
-    });
+  it('should report change of lightCtrl/0/onOff correctly', (done) => {
+    so.onChange('lightCtrl/0/onOff', (cVal, pVal) => {
+      if (cVal === 0 && pVal === 1) { done() }
+    })
+    so.write('lightCtrl', 0, 'onOff', 0, (err, data) => {
+      expect(err).to.be.equal(null)
+      // console.log(err);
+      // console.log(data);
+    })
+  })
 
-    it('should report change of led1/onOff correctly', function (done) {
-        so.onChange('led1/onOff', function (cVal, pVal) {
-            if (cVal === 0 && pVal === 1)
-                done();
-        });
-        so.write('lightCtrl', 1, 'onOff', 0, function (err, data) {
-            // console.log(err);
-            // console.log(data);
-        });
-    });
+  it('should report change of led0/onOff correctly', (done) => {
+    so.onChange('led0/onOff', (cVal, pVal) => {
+      if (cVal === 1 && pVal === 0) { done() }
+    })
+    so.write('lightCtrl', 0, 'onOff', 1, (err, data) => {
+      expect(err).to.be.equal(null)
+      // console.log(err);
+      // console.log(data);
+    })
+  })
 
-});
+  it('should report change of lightCtrl/1/onOff correctly', (done) => {
+    so.onChange('lightCtrl/1/onOff', (cVal, pVal) => {
+      if (cVal === 1 && pVal === 0) { done() }
+    })
+    so.write('lightCtrl', 1, 'onOff', 1, (err, data) => {
+      expect(err).to.be.equal(null)
+      // console.log(err);
+      // console.log(data);
+    })
+  })
+
+  it('should report change of led1/onOff correctly', (done) => {
+    so.onChange('led1/onOff', (cVal, pVal) => {
+      if (cVal === 0 && pVal === 1) { done() }
+    })
+    so.write('lightCtrl', 1, 'onOff', 0, (err, data) => {
+      expect(err).to.be.equal(null)
+      // console.log(err);
+      // console.log(data);
+    })
+  })
+})
